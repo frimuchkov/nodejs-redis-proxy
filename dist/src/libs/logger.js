e49e021c147c0265d84c897ec0869fe78c064b8c
andrew <afrimuchkov@yandex.ru>
2019-10-29 21:43:47 +0300

    Remove dist from repo

--- dist/src/libs/logger.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logger = console;
-exports.default = logger;
-//# sourceMappingURL=logger.js.map
--- dist/src/modules/server.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" />
-import { Server } from 'http';
-declare class HttpServer {
-    init(): Promise<Server>;
-}
-export default HttpServer;
